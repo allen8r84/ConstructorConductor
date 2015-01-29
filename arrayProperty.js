bf1e6ea4fc38513b436cfca5b4ec66e8f86f5b2d
Jon <jon@allen8r.com>
2015-01-28 20:25:45 -0700

    finish third assignment in today's repo

--- arrayProperty.js
+++ arrayProperty.js
@@ -3,3 +3,10 @@
 //Add a reverse method to the String 'class' so that every instance of String can call reverse and reverse itself.
   
   //code here
+String.prototype.reverseThisIsh = function(){
+    var string = this;
+    var newString = this.split('').reverse().join('');
+    return newString;
+}
+var newName = "this is my new name";
+newName.reverseThisIsh();
